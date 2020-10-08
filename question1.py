ac85ce04008aafa4e1ad9e80568da8b8a9b0036a
Noah Vargas <noah.vargas@daviestech.org>
2020-10-08 14:18:34 +0000

    Finished Assignment

---
+++ question1.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter a number: "))
+n2 = (n1 + 1)
+n3 = (n2 + 1)
+n4 = (n3 + 1)
+print (n2)
+print (n3)
+print (n4)
---
+++ question2.py
@@ -0,0 +1,4 @@
+d1 = float(input("Enter a decimal: "))
+d2 = float(input("Enter a decimal: "))
+d3 = float(input("Enter a decimal: "))
+print (d1 + d2 + d3)
